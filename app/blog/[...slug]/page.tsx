a58c2437065218cf2524057ce76d4bfa50c410c0
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-03-03 17:04:06 -0800

    static params

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,3 +1,11 @@
-export default function BlogPost() {
-    return <div>BlogPost Page</div>
+const getData = async (slug) => {
+  const post = await getDataFromCMS(slug);
+  return post;
+};
+
+export default async function BlogPost({ params }) {
+  const { slug } = params;
+  const post = await getData(slug);
+  
+  return <div>post</div>;
 }
