71db51c765db2bcab778820ee24c5780adaf45fb
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-03-03 19:48:36 -0800

    static params

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,11 +1,16 @@
+const delay = (time) =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(1), time);
+  });
+
 const getData = async (slug) => {
-  const post = await getDataFromCMS(slug);
-  return post;
+  const post = await delay(5000);
+  return { slug };
 };
 
 export default async function BlogPost({ params }) {
   const { slug } = params;
   const post = await getData(slug);
-  
-  return <div>post</div>;
+
+  return <div>{post.slug}</div>;
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import HeaderLink from "./HeaderLink";
 const Header = () => {
   return (
